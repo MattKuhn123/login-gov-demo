f25ba9d402ff4cdb397d81a352e59c9d246152bc
Kuhn, Matthew L <mlkuhn@tva.gov>
2022-11-15 09:03:32 -0500

    init

---
+++ src/main/java/kuhn/example/logingovdemo/LoginGovDemoApplication.java
@@ -0,0 +1,13 @@
+package kuhn.example.logingovdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginGovDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginGovDemoApplication.class, args);
+	}
+
+}
