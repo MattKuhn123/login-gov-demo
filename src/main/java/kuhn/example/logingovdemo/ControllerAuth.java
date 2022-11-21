3e9a5a617d0f9a7585872dd811f0ed2e6248e46e
Kuhn, Matthew L <mlkuhn@tva.gov>
2022-11-21 17:28:58 -0500

    ui

--- src/main/java/kuhn/example/logingovdemo/ControllerAuth.java
+++ src/main/java/kuhn/example/logingovdemo/ControllerAuth.java
@@ -1,5 +1,6 @@
 package kuhn.example.logingovdemo;
 
+import java.time.Instant;
 import java.util.Random;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.ServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,6 +27,20 @@ public class ControllerAuth {
         System.out.println("enter [/auth/random]");
         System.out.println("exit [/auth/random]");
         return String.format("(auth) %s", String.valueOf(new Random().nextInt()));
+    }
+
+    @GetMapping("/expires")
+    public String expires(final ServletRequest req) {
+        System.out.println("enter [/auth/expires]");
+        try {
+            final DecodedJWT decodedJWT = JWT.decode(CookieUtils.getCookie(req, CookieUtils.JWT_NAME));
+            System.out.println("exit [/auth/expires]");
+            return String.format("expires in: [%s]s", decodedJWT.getExpiresAtAsInstant().minusMillis(Instant.now().toEpochMilli()).getEpochSecond());
+        } catch (final Exception e) {
+            System.out.println("exit [/auth/expires]");
+            return "Unknown";
+        }
+        
     }
 
     @GetMapping("/email")
