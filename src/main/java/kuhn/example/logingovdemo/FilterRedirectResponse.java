37ea9d4be99d6baea24004fab2404a9d2d0d48e5
Kuhn, Matthew L <mlkuhn@tva.gov>
2022-11-21 08:18:44 -0500

    refactoring

--- src/main/java/kuhn/example/logingovdemo/FilterRedirectResponse.java
+++ src/main/java/kuhn/example/logingovdemo/FilterRedirectResponse.java
@@ -2,6 +2,13 @@ package kuhn.example.logingovdemo;
 
 import java.io.IOException;
 
+import java.io.InputStreamReader;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.time.Instant;
+import java.util.Base64;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,6 +22,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.util.FileCopyUtils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 @Component
 public class FilterRedirectResponse implements Filter {
@@ -69,7 +82,7 @@ public class FilterRedirectResponse implements Filter {
                 + "client_assertion=%s&"
                 + "client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&"
                 + "code=%s&"
-                + "grant_type=authorization_code", loginGovUrl, Utils.createClientAssertion(clientId, loginGovUrl), code);
+                + "grant_type=authorization_code", loginGovUrl, createClientAssertion(clientId, loginGovUrl), code);
 
         System.out.println(String.format("Request: [%s]", url));
         final TokenResponse jwtResponse = restTemplate.postForObject(url, null, TokenResponse.class);
@@ -79,5 +92,36 @@ public class FilterRedirectResponse implements Filter {
 
         System.out.println(String.format("Result: [%s]", jwtResponse.toString()));
         return jwtResponse;
+    }
+
+    private String createClientAssertion(final String clientId, final String loginGovUrl) {
+        try {
+            return JWT.create()
+                    .withIssuer(clientId)
+                    .withSubject(clientId)
+                    .withAudience(loginGovUrl + "api/openid_connect/token")
+                    .withJWTId(java.util.UUID.randomUUID().toString())
+                    .withExpiresAt(Instant.ofEpochMilli(Instant.now().toEpochMilli() + 100000))
+                    .sign(Algorithm.RSA256(getPrivateKey()));
+        } catch (Exception e) {
+            System.out.println("Something went wrong generating the client assertion");
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private RSAPrivateKey getPrivateKey() throws Exception {
+        try {
+            return (RSAPrivateKey) KeyFactory.getInstance("RSA")
+                    .generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder()
+                            .decode(FileCopyUtils
+                                    .copyToString(new InputStreamReader(
+                                            new DefaultResourceLoader().getResource(Utils.PEM_LOCATION).getInputStream()))
+                                    .replace("-----BEGIN PRIVATE KEY-----", "")
+                                    .replaceAll("\n", "").replace("-----END PRIVATE KEY-----", ""))));
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
--- src/main/java/kuhn/example/logingovdemo/Utils.java
+++ src/main/java/kuhn/example/logingovdemo/Utils.java
@@ -1,23 +1,12 @@
 package kuhn.example.logingovdemo;
 
-import java.io.InputStreamReader;
-import java.security.KeyFactory;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.time.Instant;
-import java.util.Base64;
-
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.util.FileCopyUtils;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 
 public class Utils {
     public static final String JWT_NAME = "kuhn.example.jwt";
@@ -26,42 +15,6 @@ public class Utils {
     // TODO : Replace with your pem
     public static final String PEM_LOCATION = "classpath:security\\private-kuhn-demo.pem"; 
 
-    /**
-     * Low-level stuff. Not important for the demonstration
-     * @param clientId with login.gov
-     * @param pemLocation Where your pem file is.
-     * @return the client assertion
-     */
-    public static String createClientAssertion(final String clientId, final String loginGovUrl) {
-        try {
-            return JWT.create()
-                    .withIssuer(clientId)
-                    .withSubject(clientId)
-                    .withAudience(loginGovUrl + "api/openid_connect/token")
-                    .withJWTId(java.util.UUID.randomUUID().toString())
-                    .withExpiresAt(Instant.ofEpochMilli(Instant.now().toEpochMilli() + 100000))
-                    .sign(Algorithm.RSA256(getPrivateKey()));
-        } catch (Exception e) {
-            System.out.println("Something went wrong generating the client assertion");
-            e.printStackTrace();
-            return "";
-        }
-    }
-
-    private static RSAPrivateKey getPrivateKey() throws Exception {
-        try {
-            return (RSAPrivateKey) KeyFactory.getInstance("RSA")
-                    .generatePrivate(new PKCS8EncodedKeySpec(Base64.getDecoder()
-                            .decode(FileCopyUtils
-                                    .copyToString(new InputStreamReader(
-                                            new DefaultResourceLoader().getResource(PEM_LOCATION).getInputStream()))
-                                    .replace("-----BEGIN PRIVATE KEY-----", "")
-                                    .replaceAll("\n", "").replace("-----END PRIVATE KEY-----", ""))));
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
 
     public static String getCookie(final ServletRequest request, final String key) {
         for (final Cookie c : ((HttpServletRequest)request).getCookies()) {
